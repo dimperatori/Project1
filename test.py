dca31cba8b9a3e91cf01a98a0a86622a7f36bcfb
Domingo Imperatori <nicoin2@hotmail.com>
2018-03-27 15:08:35 -0700

    Adding division

--- test.py
+++ test.py
@@ -3,8 +3,15 @@ def main():
     y=input("Please enter y:")
     z=multiply(x,y)
     print(z)
+    z=divide(x,y)
+    print(z)
 
 def multiply(x,y):
     return int(x)*int(y)
+
+
+def divide(x,y):
+    return float(x)/float(y)
+
 
 main()
